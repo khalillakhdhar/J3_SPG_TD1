710a9de697161b9dd0d8c1eebed2114860ff012f
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-02 12:25:28 +0100

    Update Formation.java

--- src/main/java/com/rappel/model/Formation.java
+++ src/main/java/com/rappel/model/Formation.java
@@ -1,8 +1,15 @@
 package com.rappel.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Formation {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+long id;
+
 
 }
