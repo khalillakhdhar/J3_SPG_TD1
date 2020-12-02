da50c4b7cf9ec53fb02efe9da02f99eea6477d33
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-02 14:33:50 +0100

    synchronisation

--- src/main/java/com/rappel/controller/UserController.java
+++ src/main/java/com/rappel/controller/UserController.java
@@ -2,9 +2,17 @@ package com.rappel.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rappel.exception.ResourceNotFoundException;
 import com.rappel.model.User;
 import com.rappel.repository.UserRepository;
 
@@ -15,7 +23,22 @@ UserRepository userRepository;
 @GetMapping("user")
 public List<User> afficheUsers()
 {
+	
 return 	(List<User>) userRepository.findAll();
 
 }
+@PostMapping("user")
+public User adduser(@Valid @RequestBody User user)
+{
+	return userRepository.save(user);
+
+}
+
+//Get a Single Utilisateur
+@GetMapping("/user/{id}")
+public User getUtilisateurById(@PathVariable(value = "id") Long id) {
+ return userRepository.findById(id)
+         .orElseThrow(() -> new ResourceNotFoundException("Utilisateur", "id", id));
+}
+
 }
