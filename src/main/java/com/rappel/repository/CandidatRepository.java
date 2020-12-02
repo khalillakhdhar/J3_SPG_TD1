14ff00c46181c400b5689d2b226b5838ebb6ba63
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-02 15:22:05 +0100

    Update CandidatRepository.java

--- src/main/java/com/rappel/repository/CandidatRepository.java
+++ src/main/java/com/rappel/repository/CandidatRepository.java
@@ -2,6 +2,8 @@ package com.rappel.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
+import com.rappel.model.Candidat;
+
 public interface CandidatRepository extends CrudRepository<Candidat, Long> {
 
 }
