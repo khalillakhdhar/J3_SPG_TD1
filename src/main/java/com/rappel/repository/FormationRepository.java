888f9687355cd2bfd167f61ea98c79ea07b5e4d2
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-02 13:39:00 +0100

    Create FormationRepository.java

---
+++ src/main/java/com/rappel/repository/FormationRepository.java
@@ -0,0 +1,9 @@
+package com.rappel.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.rappel.model.Formation;
+
+public interface FormationRepository extends CrudRepository<Formation, Long> {
+
+}
