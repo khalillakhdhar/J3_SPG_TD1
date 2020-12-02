9f1ea400a7ff08540cca50d8facd23c7598123ce
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-02 11:39:19 +0100

    Initial commit

---
+++ src/main/java/com/rappel/J3SpgTd1Application.java
@@ -0,0 +1,13 @@
+package com.rappel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J3SpgTd1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J3SpgTd1Application.class, args);
+	}
+
+}
