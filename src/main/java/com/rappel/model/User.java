e407ec9c3cd95a40cc02dc17dea35948efdfe5ab
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-02 11:48:47 +0100

    Update User.java

--- src/main/java/com/rappel/model/User.java
+++ src/main/java/com/rappel/model/User.java
@@ -1,11 +1,16 @@
 package com.rappel.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "utilisateur")
 public class User {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	long id;
 
 }
