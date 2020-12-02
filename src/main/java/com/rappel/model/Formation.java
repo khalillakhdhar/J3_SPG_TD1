09f972db6052b18a6d4c2187dd40be012c1810b1
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-02 12:29:10 +0100

    Update Formation.java

--- src/main/java/com/rappel/model/Formation.java
+++ src/main/java/com/rappel/model/Formation.java
@@ -7,14 +7,54 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
+import com.sun.istack.NotNull;
+
 @Entity
 public class Formation {
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
-long id;
+private long id;
 @NotBlank
-String titre;
+private String titre;
 @Column(nullable = true,length = 100)
-String description;
+private String description;
+@Column(unique = true,length = 30)
+private String formateur;
+@NotNull
+private int duree;
+public long getId() {
+	return id;
+}
+public void setId(long id) {
+	this.id = id;
+}
+public String getTitre() {
+	return titre;
+}
+public void setTitre(String titre) {
+	this.titre = titre;
+}
+public String getDescription() {
+	return description;
+}
+public void setDescription(String description) {
+	this.description = description;
+}
+public String getFormateur() {
+	return formateur;
+}
+public void setFormateur(String formateur) {
+	this.formateur = formateur;
+}
+public int getDuree() {
+	return duree;
+}
+public void setDuree(int duree) {
+	this.duree = duree;
+}
+
+
+
+
 
 }
